9c2b802f1734b1e031e1951faa717a88938aa516
Enrique <jjug792@aucklanduni.ac.nz>
2024-05-09 08:34:20 +1200

    modified size code for pipes

--- vhd and data/pipes.vhd
+++ vhd and data/pipes.vhd
@@ -27,11 +27,12 @@ architecture rtl of pipes is
   constant screen_height : integer := 479;
   constant screen_width  : integer := 639;
   constant half_width    : integer := 239;
-  constant size          : integer := 16;
   constant gap_size      : integer := 64;
-  constant scale         : integer := 2;
   constant x_speed       : integer := 1;
   constant distance      : integer := 319;
+
+  constant scale : integer                               := 2;
+  constant size  : ieee.numeric_std.unsigned(7 downto 0) := shift_left("00010000", scale - 1); -- 16 * 2^(scale - 1)
 
   -- Top and Bottom pipes
   signal pipe1_top_on, pipe1_bottom_on : std_logic;
@@ -53,20 +54,20 @@ begin
   pipe_on   <= s_pipe_on;
 
   -- Pipe1 : TOP AND BOTTOM
-  pipe1_top_on <= '1' when ('0' & pixel_column >= pipe1_x_pos) and ('0' & pixel_column < pipe1_x_pos + size * scale)
+  pipe1_top_on <= '1' when ('0' & pixel_column >= pipe1_x_pos) and ('0' & pixel_column < pipe1_x_pos + to_integer(size))
     and (pixel_row >= 0) and (pixel_row < pipe1_y_pos - gap_size) else
     '0';
 
-  pipe1_bottom_on <= '1' when ('0' & pixel_column >= pipe1_x_pos) and ('0' & pixel_column < pipe1_x_pos + size * scale)
+  pipe1_bottom_on <= '1' when ('0' & pixel_column >= pipe1_x_pos) and ('0' & pixel_column < pipe1_x_pos + to_integer(size))
     and (pixel_row  <= screen_height) and (pixel_row > pipe1_y_pos + gap_size) else
     '0';
 
   -- Pipe2 : TOP AND BOTTOM
-  pipe2_top_on <= '1' when ('0' & pixel_column >= pipe2_x_pos) and ('0' & pixel_column < pipe2_x_pos + size * scale)
+  pipe2_top_on <= '1' when ('0' & pixel_column >= pipe2_x_pos) and ('0' & pixel_column < pipe2_x_pos + to_integer(size))
     and (pixel_row >= 0) and (pixel_row < pipe2_y_pos - gap_size) else
     '0';
 
-  pipe2_bottom_on <= '1' when ('0' & pixel_column >= pipe2_x_pos) and ('0' & pixel_column < pipe2_x_pos + size * scale)
+  pipe2_bottom_on <= '1' when ('0' & pixel_column >= pipe2_x_pos) and ('0' & pixel_column < pipe2_x_pos + to_integer(size))
     and (pixel_row  <= screen_height) and (pixel_row > pipe2_y_pos + gap_size) else
     '0';
 
@@ -81,12 +82,12 @@ begin
       pipe1_x_pos <= pipe1_x_pos - x_speed;
       pipe2_x_pos <= pipe2_x_pos - x_speed;
 
-      if (pipe1_x_pos =- size * scale) then
+      if (pipe1_x_pos =- to_integer(size) * scale) then
         pipe1_y_pos <= to_integer(random_num) + half_width;
         pipe1_x_pos <= conv_std_logic_vector(screen_width, 11);
       end if;
 
-      if (pipe2_x_pos =- size * scale) then
+      if (pipe2_x_pos =- to_integer(size) * scale) then
         pipe2_y_pos <= to_integer(random_num) + half_width;
         pipe2_x_pos <= conv_std_logic_vector(screen_width, 11);
       end if;
