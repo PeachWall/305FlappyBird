b042b244090ec6dca544bb9ef7b588900358f42d
LachlanChan <lcha691@aucklanduni.ac.nz>
2024-05-13 13:16:39 +1200

    Added notes to collision
    Need to add abilities and a proper way to handle detection

--- vhd and data/collision.vhd
+++ vhd and data/collision.vhd
@@ -7,11 +7,14 @@ entity collision is
   port
   (
     bird_on, pipe_on, floor_on : in std_logic;
+	 -- add powerup collisions
     collided    : out std_logic
   );
 end entity collision;
 
-architecture rtl of collision is
+-- need a way to track only 1 collision each time the bird passes a pipe it has collided with
+
+architecture beh of collision is
 begin
   collided <= '1' when ((bird_on = '1' and pipe_on = '1') or (bird_on = '1' and floor_on = '1')) else
     '0';
