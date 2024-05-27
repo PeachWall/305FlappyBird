02c4b0818cc1361fcbcf4280c1f7150b8ffc08e0
Enrique <jjug792@aucklanduni.ac.nz>
2024-05-27 18:14:14 +1200

    added level-blue pipe

--- vhd and data/score_counter.vhd
+++ vhd and data/score_counter.vhd
@@ -4,20 +4,18 @@ use ieee.numeric_std.all;
 use ieee.math_real.all;
 use IEEE.STD_LOGIC_UNSIGNED.all;
 use work.util.all;
-
-
 entity score_counter is
   port
   (
     count                            : in std_logic;
-    game_state              : in std_logic_vector(3 downto 0);
+    game_state                       : in std_logic_vector(3 downto 0);
     ones_out, tens_out, hundreds_out : out std_logic_vector(6 downto 0);
     points                           : out std_logic_vector(9 downto 0)
   );
 end entity score_counter;
 
 architecture arch of score_counter is
-  signal score : integer := 0;
+  signal score          : integer := 0;
   signal cur_game_state : game_states;
 
   component three_digi_seg is
@@ -37,7 +35,7 @@ begin
   begin
     if cur_game_state = MENU then
       counter := 0; -- when we go to menu, want to reset the counter to zero for when we begin the game
-      elsif rising_edge(count) then
+    elsif rising_edge(count) then
       if (counter /= 999) then
         counter := counter + 1;
       else
--- vhd and data/speed_controller.vhd
+++ vhd and data/speed_controller.vhd
@@ -8,7 +8,7 @@ use work.util.all;
 entity speed_controller is
   port
   (
-    switches    : in std_logic_vector(9 downto 0);
+    points      : in std_logic_vector(9 downto 0);
     game_state  : in std_logic_vector(2 downto 0);
     speed_state : in std_logic_vector(2 downto 0);
     difficulty  : in std_logic_vector(2 downto 0);
@@ -26,7 +26,7 @@ begin
   cur_game_state  <= game_states'val(to_integer(unsigned(game_state)));
   cur_speed_state <= speed_states'val(to_integer(unsigned(speed_state)));
 
-  base_speed <= "010" when difficulty /= "100" else
+  base_speed <= "010" when difficulty /= "100" and points <= 10 else
     "011";
 
   speed_out <=
